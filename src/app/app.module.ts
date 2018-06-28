cd97058dc8404a5940c4e13507fe31ae6a73d0de
DiegoLoveraBD <39605076+DiegoLoveraBD@users.noreply.github.com>
2018-06-28 14:41:34 -0500

    Cambio a Material Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
